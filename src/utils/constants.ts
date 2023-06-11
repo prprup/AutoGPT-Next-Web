482d9a477833349a681d34fb73a34a14044fdb1f
yexuting <610802397@qq.com>
2023-06-11 14:05:23 +0800

    modify default DEFAULT_MAX_TOKENS

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,5 +5,5 @@ export const GPT_MODEL_NAMES = [GPT_35_TURBO, GPT_4];
 export const DEFAULT_MAX_LOOPS_FREE = 4 as const;
 export const DEFAULT_MAX_LOOPS_PAID = 16 as const;
 export const DEFAULT_MAX_LOOPS_CUSTOM_API_KEY = 50 as const;
-export const DEFAULT_MAX_TOKENS = 1200 as const;
+export const DEFAULT_MAX_TOKENS = 400 as const;
 export const DEFAULT_TEMPERATURE = 0.9 as const;
