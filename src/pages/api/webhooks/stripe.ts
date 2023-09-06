2c5e005ad22fb239fcb8cf7f49b9d344ba416157
yexuting <610802397@qq.com>
2023-09-06 17:09:50 +0800

    修改报错信息

--- src/pages/api/webhooks/stripe.ts
+++ src/pages/api/webhooks/stripe.ts
@@ -8,7 +8,7 @@ import { prisma } from "../../../server/db";
 import { getCustomerEmail } from "../../../utils/stripe-utils";
 
 const stripe = new Stripe(env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2022-11-15",
+  apiVersion: "",
 });
 
 const webhookSecret = env.STRIPE_WEBHOOK_SECRET ?? "";
